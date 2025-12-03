dd0c0f4e02901a494bdfb2e6841ab875a96ecd83
WinnardArthur <winnardarthur7@gmail.com>
2025-12-03 06:51:40 +0000

    feat(dashboard layout): are requireAuth function to protect children pages

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,10 +1,13 @@
 import { AppSidebar } from "@/components/app-sidebar";
 import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
+import { requireAuth } from "@/lib/auth-utils";
 
-const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
+  await requireAuth();
+
   return (
     <SidebarProvider>
-        <AppSidebar />
+      <AppSidebar />
       <SidebarInset>{children}</SidebarInset>
     </SidebarProvider>
   );
