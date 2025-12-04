303798a2fcff54672965aae45d502b40fd77a39a
WinnardArthur <winnardarthur7@gmail.com>
2025-12-04 06:39:10 +0000

    feat(auth): connect polar to better-auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,10 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { checkout, polar, portal } from "@polar-sh/better-auth";
 
 import prisma from "@/lib/db";
+
+import { polarClient } from "./polar";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -11,4 +14,23 @@ export const auth = betterAuth({
     enabled: true,
     autoSignIn: true,
   },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "3933e3da-101c-4164-8957-1a12e33f7b13",
+              slug: "premium", // Custom slug for easy reference in Checkout URL, e.g. /checkout/AutoBase-Premium
+            },
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+      ],
+    }),
+  ],
 });
