83828d3b058ae1c77fc0b4c1767770e6d15ea3a9
WinnardArthur <winnardarthur7@gmail.com>
2025-12-05 07:44:11 +0000

    feat(workflows page): implement creation of workflows

--- src/app/(dashboard)/(home)/workflows/page.tsx
+++ src/app/(dashboard)/(home)/workflows/page.tsx
@@ -1,5 +1,27 @@
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+import {
+  WorkflowContainer,
+  WorkflowsList,
+} from "@/features/workflows/components/workflows-list";
+import { HydrateClient } from "@/trpc/server";
+import { prefetchWorkflows } from "@/features/workflows/server/prefetch";
+
 const WorkflowsPage = () => {
-  return <div>WorkFlows</div>;
+  prefetchWorkflows();
+
+  return (
+    <WorkflowContainer>
+      <HydrateClient>
+        <ErrorBoundary fallback={<p>Something went wrong</p>}>
+          <Suspense fallback={<p>Loading...</p>}>
+            <WorkflowsList />
+          </Suspense>
+        </ErrorBoundary>
+      </HydrateClient>
+    </WorkflowContainer>
+  );
 };
 
 export default WorkflowsPage;
