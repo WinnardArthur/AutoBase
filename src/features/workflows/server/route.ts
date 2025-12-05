7577c08dfa5219698a66a5103abfc5ad82599f19
WinnardArthur <winnardarthur7@gmail.com>
2025-12-05 20:32:21 +0000

    feat: setup pagination

--- src/features/workflows/server/route.ts
+++ src/features/workflows/server/route.ts
@@ -1,8 +1,13 @@
 import { generateSlug } from "random-word-slugs";
 
 import prisma from "@/lib/db";
-import { createTRPCRouter, premiumProcedure, protectedProcedure } from "@/trpc/init";
+import {
+  createTRPCRouter,
+  premiumProcedure,
+  protectedProcedure,
+} from "@/trpc/init";
 import z from "zod";
+import { PAGINATION } from "@/config/constants";
 
 export const workflowRouter = createTRPCRouter({
   // Create Workflow
@@ -32,15 +37,56 @@ export const workflowRouter = createTRPCRouter({
     }),
 
   // Get Workflows
-  getMany: protectedProcedure.query(({ ctx }) => {
-    const workflows = prisma.workflow.findMany({
-      where: {
-        userId: ctx.auth.user.id,
-      },
-    });
+  getMany: protectedProcedure
+    .input(
+      z.object({
+        page: z.number().default(PAGINATION.DEFAULT_PAGE),
+        pageSize: z
+          .number()
+          .min(PAGINATION.MIN_PAGE_SIZE)
+          .max(PAGINATION.MAX_PAGE_SIZE)
+          .default(PAGINATION.DEFAULT_PAGE_SIZE),
+        search: z.string().default(""),
+      })
+    )
+    .query(async ({ ctx, input: { page, pageSize, search } }) => {
+      const [items, totalCount] = await Promise.all([
+        prisma.workflow.findMany({
+          skip: (page - 1) * pageSize,
+          take: pageSize,
+          where: {
+            userId: ctx.auth.user.id,
+            name: {
+              contains: search,
+              mode: "insensitive",
+            },
+          },
+          orderBy: {
+            updatedAt: "desc",
+          },
+        }),
 
-    return workflows;
-  }),
+        prisma.workflow.count({
+          where: {
+            userId: ctx.auth.user.id,
+          },
+        }),
+      ]);
+
+      const totalPages = Math.ceil(totalCount / pageSize);
+      const hasNextPage = page < totalPages;
+      const hasPreviousPage = page > 1;
+
+      return {
+        items,
+        page,
+        pageSize,
+        totalCount,
+        totalPages,
+        hasNextPage,
+        hasPreviousPage,
+      };
+    }),
 
   // Remove/Delete a workflow
   remove: protectedProcedure
