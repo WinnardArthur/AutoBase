4e1cf8c1046934b9934297db60e7f63a83216c84
WinnardArthur <winnardarthur7@gmail.com>
2025-12-07 13:18:05 +0000

    feat(workflow prefetch): prefetch a single workflow

--- src/features/workflows/server/prefetch.ts
+++ src/features/workflows/server/prefetch.ts
@@ -1,11 +1,19 @@
-import type { inferInput } from '@trpc/tanstack-react-query';
-import { prefetch, trpc } from '@/trpc/server';
+import type { inferInput } from "@trpc/tanstack-react-query";
+import { prefetch, trpc } from "@/trpc/server";
 
-type Input = inferInput<typeof trpc.workflows.getMany>;
+type GetManyInput = inferInput<typeof trpc.workflows.getMany>;
+export type GetOneInput = inferInput<typeof trpc.workflows.getOne>;
 
 /**
  * Prefetch all Workflows
  */
-export const prefetchWorkflows = (params: Input) => {
-    return prefetch(trpc.workflows.getMany.queryOptions(params))
-}
+export const prefetchWorkflows = (params: GetManyInput) => {
+  return prefetch(trpc.workflows.getMany.queryOptions(params));
+};
+
+/**
+ * Prefetch a single workflow by ID
+ */
+export const prefetchWorkflow = (params: GetOneInput) => {
+  return prefetch(trpc.workflows.getOne.queryOptions(params));
+};
