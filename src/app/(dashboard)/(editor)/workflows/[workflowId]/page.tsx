fa8d17f77f13b85df9419f1441bad6031dc23d25
WinnardArthur <winnardarthur7@gmail.com>
2025-12-07 13:19:34 +0000

    feat: add editor and setup initial page

--- src/app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
+++ src/app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
@@ -1,3 +1,13 @@
+import {
+  Editor,
+  EditorError,
+  EditorLoading,
+} from "@/features/editor/components/editor";
+import { prefetchWorkflow } from "@/features/workflows/server/prefetch";
+import { HydrateClient } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
 interface WorkflowIdPageProps {
   params: Promise<{ workflowId: string }>;
 }
@@ -5,7 +15,17 @@ interface WorkflowIdPageProps {
 const WorkflowIdPage = async ({ params }: WorkflowIdPageProps) => {
   const { workflowId } = await params;
 
-  return <div>Workflow ID Page ---- {workflowId}</div>;
+  prefetchWorkflow({ id: workflowId });
+
+  return (
+    <HydrateClient>
+      <ErrorBoundary fallback={<EditorError />}>
+        <Suspense fallback={<EditorLoading />}>
+          <Editor workflowId={workflowId} />
+        </Suspense>
+      </ErrorBoundary>
+    </HydrateClient>
+  );
 };
 
 export default WorkflowIdPage;
