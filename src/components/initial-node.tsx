6bd29b0a314d436af4920710497a2646568661e4
WinnardArthur <winnardarthur7@gmail.com>
2025-12-09 08:51:46 +0000

    feat: create initial-node component

---
+++ src/components/initial-node.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { memo } from "react";
+import { PlusIcon } from "lucide-react";
+import type { NodeProps } from "@xyflow/react";
+
+import { PlaceholderNode } from "./react-flow/placeholder-node";
+import { WorkflowNode } from "./workflow-node";
+
+export const InitialNode = memo((props: NodeProps) => {
+  return (
+    <WorkflowNode showToolbar={true }>
+      <PlaceholderNode {...props} onClick={() =>{}}>
+        <div className="cursor-pointer flex items-center justify-center">
+          <PlusIcon className="size-4" />
+        </div>
+      </PlaceholderNode>
+    </WorkflowNode>
+  );
+});
