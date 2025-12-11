17617b0877916ce8673e42241d2d8b182a9e50cd
WinnardArthur <winnardarthur7@gmail.com>
2025-12-11 09:24:11 +0000

    feat(manual-trigger): create manual trigger dialog

--- src/features/triggers/components/manual-trigger/node.tsx
+++ src/features/triggers/components/manual-trigger/node.tsx
@@ -1,16 +1,24 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 import { NodeProps } from "@xyflow/react";
 import { MousePointerIcon } from "lucide-react";
 
 import { BaseTriggerNode } from "../base-trigger-node";
+import { ManualTriggerDialog } from "./dialog";
 
 export const ManualTriggerNode = memo((props: NodeProps) => {
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  const handleOpenSettings = () => setDialogOpen(true);
+
   return (
     <>
+      <ManualTriggerDialog open={dialogOpen} onOpenChange={setDialogOpen} />
       <BaseTriggerNode
         {...props}
         icon={MousePointerIcon}
         name="When clicking 'Execute workflow'"
+        onSettings={handleOpenSettings}
+        onDoubleClick={handleOpenSettings}
       />
     </>
   );
